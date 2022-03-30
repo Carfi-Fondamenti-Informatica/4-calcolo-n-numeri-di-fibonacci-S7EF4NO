2a902d5c8d1f9224aa19a4f24c19bb39b5a60385
S7EF4NO <100076214+S7EF4NO@users.noreply.github.com>
2022-03-30 14:26:30 +0200

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -2,5 +2,21 @@
 using namespace std;
 
 int main() {
-   return 0;
+
+    int primo, secondo, somma;
+    int n;
+    primo = 1;
+    secondo=0;
+    cin >> n;
+    if (n >= 2) {
+        for (int i = 0; i < n ; i++)
+        { somma = primo + secondo,
+            primo = secondo;
+            secondo = somma;
+            cout << somma << endl;}
+
+        } else {
+            cout << "errore" << endl;
+        }
+        return 0;
 }
